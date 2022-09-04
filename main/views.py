1ebed8e9afcd0713f0af6001121a9a9fa2bb35f9
amalKuttuz <63414371+amalKuttuz@users.noreply.github.com>
2022-09-04 19:49:59 +0530

    Added more operations

--- main/views.py
+++ main/views.py
@@ -9,10 +9,30 @@ def add(request):
 
     
     # if(request.method=='post'):
-    search=int(request.POST['search'])
-    # try:
-    result=search*search
-    # except:
-    #     return HttpResponse("exception")
+    a=int(request.POST['a'])
+    b=int(request.POST['b'])
+    s=(request.POST['s'])
 
-    return render(request, 'results.html',{'result':result})
+    if(s=='+'):
+        result=a+b
+        return render(request, 'results.html', {'result': result})
+    elif(s=='-'):
+        result=a-b
+        return render(request, 'results.html', {'result': result})
+    elif(s=='*'):
+        result=a*b
+        return render(request, 'results.html', {'result': result})
+    elif(s=='/'):
+        result=a/b
+        return render(request, 'results.html', {'result': result})
+    elif(s=='%'):
+        result=a%b
+        return render(request, 'results.html', {'result': result})
+    elif (s == ''):
+        return HttpResponse("operator box is empty")
+
+    else:
+        return HttpResponse("Enter a valid operator")
+
+
+    # return render(request, 'results.html',{'result':result})
